072cddcfb61eeeb3b54d0f9a05785e62e18dac66
sexyseunghwan <ssh9308@naver.com>
2024-08-29 22:18:14 +0900

    2024-08-29
    2024-08-29

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Create date : 2024-07-12
 Description : Code that can perform various functions through Telegram
     
 History     : 2024-07-12 Seunghwan Shin       # first creat
-              2024-08-26 Seunghwan Shin       # Modified so that only integers can be represented on the x-axis of the graph.
+              2024-08-27 Seunghwan Shin       # Modified so that only integers can be represented on the x-axis of the graph.
 """
 from controller import *
 
